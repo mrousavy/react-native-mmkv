d6630a8fbd7e62680a6dee8f051f2ed1cec8d08a
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-24 11:38:57 +0100

    cast jsi::Runtime

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -1,8 +1,20 @@
 #include <jni.h>
 #include <android/log.h>
+#include "MMKV/Core/MMKV.h"
+#include <jsi/jsi.h>
+
+using namespace facebook;
+
+void install(jsi::Runtime& runtime) {
+
+}
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_reactnativemmkv_MmkvModule_nativeInstall(JNIEnv *env, jobject clazz, jlong jsiPtr) {
     __android_log_print(ANDROID_LOG_VERBOSE, "react-native-leveldb", "Initializing react-native-mmkv");
+    MMKV::initializeMMKV(nullptr);
+
+    auto runtime = reinterpret_cast<jsi::Runtime*>(jsiPtr);
+    install(*runtime);
 }
