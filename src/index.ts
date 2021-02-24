1a345f72c40dc8314394426646ffcd07171f7d7f
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-24 10:08:23 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ const g = global as any;
 
 export const MMKV = {
   set: g.mmkvSet as (value: boolean | string | number, key: string) => void,
-  getBoolean: g.getBoolean as (key: string) => boolean,
-  getString: g.getString as (key: string) => string,
-  getNumber: g.getNumber as (key: string) => number,
+  getBoolean: g.mmkvGetBoolean as (key: string) => boolean,
+  getString: g.mmkvGetString as (key: string) => string,
+  getNumber: g.mmkvGetNumber as (key: string) => number,
+  delete: g.mmkvDelete as (key: string) => void,
+  getAllKeys: g.mmkvGetAllKeys as () => string[],
 };
