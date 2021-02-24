ef0215eb87c8d0d1abaaba3d1e759eec09e2cc7d
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-24 11:10:04 +0100

    Update MmkvModule.kt

--- android/src/main/java/com/reactnativemmkv/MmkvModule.kt
+++ android/src/main/java/com/reactnativemmkv/MmkvModule.kt
@@ -2,8 +2,6 @@ package com.reactnativemmkv
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
 
 class MmkvModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
   override fun getName(): String {
