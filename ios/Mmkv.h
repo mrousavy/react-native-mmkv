9cbdbd4be02b9ae82b2a8f47e561daba2a0e3a34
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-24 09:53:23 +0100

    Install setValue

--- ios/Mmkv.h
+++ ios/Mmkv.h
@@ -1,11 +1,5 @@
 #import <React/RCTBridgeModule.h>
 
-#ifdef __cplusplus
-
-#import "mmkv-wrapper.h"
-
-#endif
-
 @interface Mmkv : NSObject <RCTBridgeModule>
 
 @property (nonatomic, assign) BOOL setBridgeOnMainQueue;
--- ios/Mmkv.mm
+++ ios/Mmkv.mm
@@ -1,6 +1,10 @@
 #import "Mmkv.h"
 #import <React/RCTBridge+Private.h>
 #import <React/RCTUtils.h>
+#import <MMKV/MMKV.h>
+#import <jsi/jsi.h>
+
+using namespace facebook;
 
 @implementation Mmkv
 @synthesize bridge = _bridge;
@@ -10,6 +14,31 @@ RCT_EXPORT_MODULE()
 
 + (BOOL)requiresMainQueueSetup {
     return YES;
+}
+
+static NSString *convertJSIStringToNSString(jsi::Runtime &runtime, const jsi::String &value)
+{
+    return [NSString stringWithUTF8String:value.utf8(runtime).c_str()];
+}
+
+- (void)install:(jsi::Runtime &)jsiRuntime
+{
+    auto mmkvSet = jsi::Function::createFromHostFunction(jsiRuntime,
+                                                         jsi::PropNameID::forAscii(jsiRuntime, "mmkvSet"),
+                                                         2,  // value, key
+                                                         [](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+        if (!arguments[1].isString()) {
+            return jsi::JSError(runtime, "Second argument ('key') has to be of type string!");
+            return jsi::Value(-1);
+        }
+        auto keyName = convertJSIStringToNSString(runtime, arguments[1].getString(runtime));
+        
+        [MMKV setValue:0 forKey:keyName];
+        
+        return jsi::Value::undefined();
+    });
+    
+    jsiRuntime.global().setProperty(jsiRuntime, "mmkvSet", std::move(mmkvSet));
 }
 
 - (void)setBridge:(RCTBridge *)bridge
@@ -22,12 +51,14 @@ RCT_EXPORT_MODULE()
         return;
     }
     
-    //[MMKV initializeMMKV:nil];
-    mmkv::setup(*(facebook::jsi::Runtime *)cxxBridge.runtime);
+    [MMKV initializeMMKV:nil];
+    
+    
+    [self install:*(jsi::Runtime *)cxxBridge.runtime];
 }
 
 - (void)invalidate {
-    mmkv::teardown();
+    //[MMKV ]
 }
 
 /*
