4c60fd5f565689718f8f501d2f3fa4b9a6778bfc
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-25 17:10:47 +0100

    Rewrite Kotlin to Java

---
+++ android/src/main/java/com/reactnativemmkv/MmkvModule.java
@@ -0,0 +1,34 @@
+package com.reactnativemmkv;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+public class MmkvModule extends ReactContextBaseJavaModule {
+  static {
+    System.loadLibrary("cpp");
+  }
+
+  private native void nativeInstall(long jsiPtr, String path);
+
+  public MmkvModule(ReactApplicationContext context) {
+    super(context);
+  }
+
+  @NonNull
+  @Override
+  public String getName() {
+    return "MMKV";
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+
+    nativeInstall(
+      this.getReactApplicationContext().getJavaScriptContextHolder().get(),
+      this.getReactApplicationContext().getFilesDir().getAbsolutePath() + "/mmkv"
+    );
+  }
+}
