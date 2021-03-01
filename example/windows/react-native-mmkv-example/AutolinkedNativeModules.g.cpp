bc6a4f15dcb4dd22dd118b40f263922b64022ac6
mrousavy <mrousavy@donotreply.github.com>
2021-03-01 22:10:14 +0100

    Autolink RN-AsyncStorage

--- example/windows/react-native-mmkv-example/AutolinkedNativeModules.g.cpp
+++ example/windows/react-native-mmkv-example/AutolinkedNativeModules.g.cpp
@@ -3,12 +3,16 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from @react-native-async-storage/async-storage
+#include <winrt/ReactNativeAsyncStorage.h>
+
 namespace winrt::Microsoft::ReactNative
 {
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
-    UNREFERENCED_PARAMETER(packageProviders);
+    // IReactPackageProviders from @react-native-async-storage/async-storage
+    packageProviders.Append(winrt::ReactNativeAsyncStorage::ReactPackageProvider());
 }
 
 }
