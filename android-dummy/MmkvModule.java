4da2f4fb7654a49bdafb943f8bcf33454e874459
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-22 12:20:27 +0100

    Remove android-dummy

--- android-dummy/MmkvModule.java
+++
@@ -1,9 +0,0 @@
-package com.reactnativemmkv;
-
-import com.facebook.react.bridge.JavaScriptContextHolder;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.reactnativemmkv.MmkvModule;
-
-public class MmkvModule extends ReactContextBaseJavaModule {
-  // dummy module
-}
