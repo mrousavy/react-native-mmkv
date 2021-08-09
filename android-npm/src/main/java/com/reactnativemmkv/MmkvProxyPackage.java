67cd1124dd0abb0587329d296bdb9d9d8db6fec1
Marc Rousavy <marcrousavy@hotmail.com>
2021-08-09 10:06:49 +0200

    fix: Create proxy npm package

---
+++ android-npm/src/main/java/com/reactnativemmkv/MmkvProxyPackage.java
@@ -0,0 +1,15 @@
+package com.reactnativemmkv;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.reactnativemmkv.MmkvPackage;
+
+public class MmkvPackage implements ReactPackage {
+  // dummy package
+}
