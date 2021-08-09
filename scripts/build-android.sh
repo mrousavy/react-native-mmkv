a6bdaa11435be2affea7b7a1620e9cf5ee6152cb
Marc Rousavy <marcrousavy@hotmail.com>
2021-08-09 10:21:50 +0200

    fix moving

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -11,9 +11,9 @@ cd android
 echo "Cleaned."
 
 ./gradlew assembleDebug
-mv build/outputs/aar/android-debug.aar android-npm
+mv build/outputs/aar/android-debug.aar ../android-npm/react-native-mmkv-debug.aar
 echo "Built debug."
 
 ./gradlew assembleRelease
-mv build/outputs/aar/android-release.aar android-npm
+mv build/outputs/aar/android-release.aar ../android-npm/react-native-mmkv-release.aar
 echo "Built release."
