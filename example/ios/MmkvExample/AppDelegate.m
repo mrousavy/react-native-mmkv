96ae8e370004dfdf3a38be2329bc2c008cabee60
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-04 16:36:43 +0200

    upgrade example app to RN 0.65.1

--- example/ios/MmkvExample/AppDelegate.m
+++ example/ios/MmkvExample/AppDelegate.m
@@ -20,8 +20,11 @@
                                                    moduleName:@"MmkvExample"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  if (@available(iOS 13.0, *)) {
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    rootView.backgroundColor = [UIColor whiteColor];
+  }
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
