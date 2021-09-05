62970bb8706e9c57536b3530c29b9f13f56ecd4b
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-05 15:48:27 +0200

    Add support for custom IDs

--- src/createMMKV.web.ts
+++ src/createMMKV.web.ts
@@ -1,17 +1,18 @@
 import type { MMKVConfiguration, MMKVInterface } from 'react-native-mmkv';
 
-// @ts-expect-error global func is a native JSI func
 export const createMMKV = (config: MMKVConfiguration): MMKVInterface => {
-  // TODO: Implement custom ID
+  const prefix = config.id;
   // TODO: Implement Encryption
   // TODO: Implement custom Path?
   return {
     clearAll: () => localStorage.clear(),
-    delete: (key) => localStorage.removeItem(key),
-    set: (key, value) => localStorage.setItem(key, value),
-    getString: (key) => localStorage.getItem(key),
-    getNumber: (key) => Number(localStorage.getItem(key)),
-    getBoolean: (key) => Boolean(localStorage.getItem(key)),
-    getAllKeys: () => Object.keys(localStorage),
+    delete: (key) => localStorage.removeItem(`${prefix}.${key}`),
+    set: (key, value) => localStorage.setItem(`${prefix}.${key}`, value),
+    getString: (key) => localStorage.getItem(`${prefix}.${key}`) ?? undefined,
+    getNumber: (key) => Number(localStorage.getItem(`${prefix}.${key}`) ?? 0),
+    getBoolean: (key) =>
+      Boolean(localStorage.getItem(`${prefix}.${key}`) ?? false),
+    getAllKeys: () =>
+      Object.keys(localStorage).map((key) => key.replace(`${prefix}.`, '')),
   };
 };
