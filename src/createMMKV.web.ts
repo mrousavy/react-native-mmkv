af6aec03cead3e5ba7703791fbda539951e09ffb
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-05 15:54:04 +0200

    Make web better

--- src/createMMKV.web.ts
+++ src/createMMKV.web.ts
@@ -1,18 +1,26 @@
+/* global localStorage */
+import { Platform } from 'react-native';
 import type { MMKVConfiguration, MMKVInterface } from 'react-native-mmkv';
 
 export const createMMKV = (config: MMKVConfiguration): MMKVInterface => {
+  // @ts-expect-error
+  const storage = global.localStorage ?? window.localStorage ?? localStorage;
+  if (storage == null) {
+    throw new Error(
+      `Could not find 'localStorage' instance! Platform: ${Platform.OS}`
+    );
+  }
   const prefix = config.id;
   // TODO: Implement Encryption
   // TODO: Implement custom Path?
   return {
-    clearAll: () => localStorage.clear(),
-    delete: (key) => localStorage.removeItem(`${prefix}.${key}`),
-    set: (key, value) => localStorage.setItem(`${prefix}.${key}`, value),
-    getString: (key) => localStorage.getItem(`${prefix}.${key}`) ?? undefined,
-    getNumber: (key) => Number(localStorage.getItem(`${prefix}.${key}`) ?? 0),
-    getBoolean: (key) =>
-      Boolean(localStorage.getItem(`${prefix}.${key}`) ?? false),
+    clearAll: () => storage.clear(),
+    delete: (key) => storage.removeItem(`${prefix}.${key}`),
+    set: (key, value) => storage.setItem(`${prefix}.${key}`, value),
+    getString: (key) => storage.getItem(`${prefix}.${key}`) ?? undefined,
+    getNumber: (key) => Number(storage.getItem(`${prefix}.${key}`) ?? 0),
+    getBoolean: (key) => Boolean(storage.getItem(`${prefix}.${key}`) ?? false),
     getAllKeys: () =>
-      Object.keys(localStorage).map((key) => key.replace(`${prefix}.`, '')),
+      Object.keys(storage).map((key) => key.substring(prefix.length + 1)),
   };
 };
