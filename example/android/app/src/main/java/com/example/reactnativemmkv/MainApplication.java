289fdbdf7c766947a3ebd922edd77620a5e49019
Marc Rousavy <marcrousavy@hotmail.com>
2021-10-07 11:57:02 +0200

    fix: Avoid duplicate C++ symbols by using `libjsi.so` shared library (#175)
    * fix: Avoid duplicate C++ symbols by using `libjsi.so` shared library
    
    * exclude `fbjni` and `jsi` from packaging options
    
    * Update build.gradle
    
    * upgrade to RN 0.66
    
    * Add `@typescript-eslint/eslint-plugin`
    
    * Upgrade RN
    
    * rebuild lockfiles

--- example/android/app/src/main/java/com/example/reactnativemmkv/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativemmkv/MainApplication.java
@@ -48,34 +48,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager()); // Remove this line if you don't want Flipper enabled
-  }
-
-  /**
-   * Loads Flipper in React Native templates.
-   *
-   * @param context
-   */
-  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.reactnativemmkvExample.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
   }
 }
