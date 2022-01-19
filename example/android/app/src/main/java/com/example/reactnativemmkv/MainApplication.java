9eea2a44e872ac66d6e22d5fa97dd3c1c7a6bde6
Marc Rousavy <me@mrousavy.com>
2022-01-19 10:13:44 +0100

    Update Android example

--- example/android/app/src/main/java/com/example/reactnativemmkv/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativemmkv/MainApplication.java
@@ -1,18 +1,14 @@
 package com.example.reactnativemmkv;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.reactnativemmkv.MmkvModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -31,11 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new MmkvModulePackage();
         }
       };
 
