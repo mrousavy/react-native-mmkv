4add4e9700b2c394a1b16ef05b10d8fc3232900a
Marc Rousavy <me@mrousavy.com>
2022-02-02 09:43:45 +0100

    fix: Mark ctor as explicit

--- android/src/main/cpp/MmkvHostObject.h
+++ android/src/main/cpp/MmkvHostObject.h
@@ -15,8 +15,8 @@ using namespace facebook;
 
 class JSI_EXPORT MmkvHostObject: public jsi::HostObject {
 public:
-  MmkvHostObject(const std::string& instanceId, std::string path, std::string cryptKey);
-  MmkvHostObject(MMKV* instance);
+  explicit MmkvHostObject(const std::string& instanceId, std::string path, std::string cryptKey);
+  explicit MmkvHostObject(MMKV* instance);
 
 public:
   jsi::Value get(jsi::Runtime&, const jsi::PropNameID& name) override;
