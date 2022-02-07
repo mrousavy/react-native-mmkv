638aabc13af359eafa5c7af0a98bf1390bc2d157
mrousavy <marcrousavy@hotmail.com>
2022-02-07 11:53:40 +0100

    fix: Add stub for Web

--- src/createMMKV.web.ts
+++ src/createMMKV.web.ts
@@ -38,5 +38,6 @@ export const createMMKV = (config: MMKVConfiguration): NativeMMKV => {
     getBoolean: (key) => Boolean(storage().getItem(key) ?? false),
     getAllKeys: () => Object.keys(storage()),
     contains: (key) => storage().getItem(key) != null,
+    encrypt: (key) => throw new Error("`encrypt(..)` is not supported on Web!")
   };
 };
