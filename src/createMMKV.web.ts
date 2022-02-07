a514da502d70c6d5f091b502b62273c3c8f665be
mrousavy <marcrousavy@hotmail.com>
2022-02-07 11:53:58 +0100

    Update createMMKV.web.ts

--- src/createMMKV.web.ts
+++ src/createMMKV.web.ts
@@ -38,6 +38,8 @@ export const createMMKV = (config: MMKVConfiguration): NativeMMKV => {
     getBoolean: (key) => Boolean(storage().getItem(key) ?? false),
     getAllKeys: () => Object.keys(storage()),
     contains: (key) => storage().getItem(key) != null,
-    encrypt: (key) => throw new Error("`encrypt(..)` is not supported on Web!")
+    encrypt: () => {
+      throw new Error("`encrypt(..)` is not supported on Web!");
+    },
   };
 };
