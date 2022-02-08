090e2b9e0e7233c91734fd3c96d8fa1729e93a46
Marc Rousavy <me@mrousavy.com>
2022-02-08 11:38:31 +0100

    feat: Implement `toJSON()`

--- src/MMKV.ts
+++ src/MMKV.ts
@@ -209,6 +209,11 @@ export class MMKV implements MMKVInterface {
   toString(): string {
     return `MMKV (${this.id}): [${this.getAllKeys().join(', ')}]`;
   }
+  toJSON(): object {
+    return {
+      [this.id]: this.getAllKeys(),
+    };
+  }
 
   addOnValueChangedListener(onValueChanged: (key: string) => void): Listener {
     this.onValueChangedListeners.push(onValueChanged);
