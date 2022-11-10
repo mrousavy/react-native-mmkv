55231b1e3329799d61e88c4be9e5f52a65cb5b76
Marc Rousavy <me@mrousavy.com>
2022-11-10 14:33:24 +0100

    fix: Manually link `MmkvPackage` in Example app

--- example/android/app/src/main/java/com/mrousavy/reactnativemmkv/MainApplication.java
+++ example/android/app/src/main/java/com/mrousavy/reactnativemmkv/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.reactnativemmkv.MmkvPackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -23,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new MmkvPackage());
           return packages;
         }
 
