df0157740050c13a36bd4b4eae549c758b470e7a
Marc Rousavy <me@mrousavy.com>
2023-04-07 11:41:00 +0200

    chore: Format

--- src/createMMKV.web.ts
+++ src/createMMKV.web.ts
@@ -5,7 +5,7 @@ import { createTextEncoder } from './createTextEncoder';
 const canUseDOM =
   typeof window !== 'undefined' && window.document?.createElement != null;
 
-const KEY_WILDCARD = "\\"
+const KEY_WILDCARD = '\\';
 
 export const createMMKV = (config: MMKVConfiguration): NativeMMKV => {
   if (config.encryptionKey != null) {
@@ -32,29 +32,33 @@ export const createMMKV = (config: MMKVConfiguration): NativeMMKV => {
   const textEncoder = createTextEncoder();
 
   if (config.id.includes(KEY_WILDCARD)) {
-    throw new Error('MMKV: `id` cannot contain the backslash character (`\\`)!');
+    throw new Error(
+      'MMKV: `id` cannot contain the backslash character (`\\`)!'
+    );
   }
 
-  const keyPrefix = `${config.id}${KEY_WILDCARD}` // mmkv.default\\
+  const keyPrefix = `${config.id}${KEY_WILDCARD}`; // mmkv.default\\
   const prefixedKey = (key: string) => {
     if (key.includes('\\')) {
-      throw new Error('MMKV: `key` cannot contain the backslash character (`\\`)!');
+      throw new Error(
+        'MMKV: `key` cannot contain the backslash character (`\\`)!'
+      );
     }
-    return `${keyPrefix}${key}`
-  }
+    return `${keyPrefix}${key}`;
+  };
 
   return {
     clearAll: () => {
-      const keys = Object.keys(storage())
+      const keys = Object.keys(storage());
       for (const key of keys) {
         if (key.startsWith(keyPrefix)) {
-          storage().removeItem(key)
+          storage().removeItem(key);
         }
       }
     },
     delete: (key) => storage().removeItem(prefixedKey(key)),
     set: (key, value) => {
-      storage().setItem(prefixedKey(key), value.toString())
+      storage().setItem(prefixedKey(key), value.toString());
     },
     getString: (key) => storage().getItem(prefixedKey(key)) ?? undefined,
     getNumber: (key) => {
@@ -74,7 +78,7 @@ export const createMMKV = (config: MMKVConfiguration): NativeMMKV => {
     },
     getAllKeys: () => {
       const keys = Object.keys(storage());
-      return keys.filter(key => key.startsWith(keyPrefix));
+      return keys.filter((key) => key.startsWith(keyPrefix));
     },
     contains: (key) => storage().getItem(prefixedKey(key)) != null,
     recrypt: () => {
