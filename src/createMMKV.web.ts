15dabfa6c703f7153a3d7e2c77423138f31be115
Hirbod <504909+hirbod@users.noreply.github.com>
2023-06-23 13:10:44 +0200

    fix: prevent console.warn spam in server environment

--- src/createMMKV.web.ts
+++ src/createMMKV.web.ts
@@ -26,8 +26,9 @@ export const createMMKV = (config: MMKVConfiguration): NativeMMKV => {
   if (config.path != null) {
     throw new Error("MMKV: 'path' is not supported on Web!");
   }
-
-  if (!hasAccessToLocalStorage()) {
+  
+  // The typeof window check prevents spam in Node server environments, such as Next.js server side props.
+  if (!hasAccessToLocalStorage() && typeof window !== "undefined") {
     console.warn(
       'MMKV: LocalStorage has been disabled. Your experience will be limited to in-memory storage!'
     );
