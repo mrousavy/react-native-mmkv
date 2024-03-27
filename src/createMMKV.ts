5a4319e6d1b087ecb032ee26c90fef75a4ea312b
Marc Rousavy <me@mrousavy.com>
2024-03-27 12:23:13 +0100

    fix: Lazily create TurboModule

--- src/createMMKV.ts
+++ src/createMMKV.ts
@@ -2,9 +2,8 @@ import type { Configuration } from './MMKV';
 import type { NativeMMKV } from './Types';
 import { getMMKVTurboModule } from './NativeMmkv';
 
-const module = getMMKVTurboModule();
-
 export const createMMKV = (config: Configuration): NativeMMKV => {
+  const module = getMMKVTurboModule();
   const instance = module.createMMKV(config);
   if (__DEV__) {
     if (typeof instance !== 'object' || instance == null) {
