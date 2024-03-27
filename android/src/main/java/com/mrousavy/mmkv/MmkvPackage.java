5ebd3ba647c380a38e296fbe753a450255ce00ee
Marc Rousavy <me@mrousavy.com>
2024-03-27 11:29:42 +0100

    Delete Java part

--- android/src/main/java/com/mrousavy/mmkv/MmkvPackage.java
+++ android/src/main/java/com/mrousavy/mmkv/MmkvPackage.java
@@ -16,29 +16,11 @@ public class MmkvPackage extends TurboReactPackage {
   @Nullable
   @Override
   public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-    if (name.equals(MmkvModule.NAME)) {
-      return new MmkvModule(reactContext);
-    } else {
-      return null;
-    }
+    return null;
   }
 
   @Override
   public ReactModuleInfoProvider getReactModuleInfoProvider() {
-    return () -> {
-      final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
-      moduleInfos.put(
-              MmkvModule.NAME,
-              new ReactModuleInfo(
-                      MmkvModule.NAME,
-                      MmkvModule.NAME,
-                      false, // canOverrideExistingModule
-                      false, // needsEagerInit
-                      true, // hasConstants
-                      false, // isCxxModule
-                      true // isTurboModule
-      ));
-      return moduleInfos;
-    };
+    return HashMap::new;
   }
 }
