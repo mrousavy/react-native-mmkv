a1616119c65d2cb45af5b5e59d738bdffbfa1f50
Marc Rousavy <me@mrousavy.com>
2024-04-30 16:57:35 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native', 'prettier'],
+  extends: ['@react-native', 'plugin:prettier/recommended'],
   rules: {
     'prettier/prettier': ['warn'],
   },
