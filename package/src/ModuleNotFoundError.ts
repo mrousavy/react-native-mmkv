efedbdfd45c61322587b3f2edbef25fe1f613dc6
Marc Rousavy <me@mrousavy.com>
2024-04-30 18:22:24 +0200

    fix: Improve error message

--- package/src/ModuleNotFoundError.ts
+++ package/src/ModuleNotFoundError.ts
@@ -7,6 +7,29 @@ declare global {
 
 const BULLET_POINT = '\n* ';
 
+function messageWithSuggestions(
+  message: string,
+  suggestions: string[]
+): string {
+  return message + BULLET_POINT + suggestions.join(BULLET_POINT);
+}
+
+function getFrameworkType(): 'react-native' | 'expo' | 'expo-go' {
+  // check if Expo
+  const ExpoConstants =
+    NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants;
+  if (ExpoConstants != null) {
+    if (ExpoConstants.appOwnership === 'expo') {
+      // We're running Expo Go
+      return 'expo-go';
+    } else {
+      // We're running Expo bare / standalone
+      return 'expo';
+    }
+  }
+  return 'react-native';
+}
+
 export class ModuleNotFoundError extends Error {
   constructor(cause?: unknown) {
     // TurboModule not found, something went wrong!
@@ -14,13 +37,26 @@ export class ModuleNotFoundError extends Error {
       // TurboModules are not available/new arch is not enabled.
       // react-native-mmkv 3.x.x requires new arch (react-native >0.74)
       // react-native-mmkv 2.x.x works on old arch (react-native <0.74)
-      throw new Error(
-        'Failed to create a new MMKV instance: react-native-mmkv 3.x.x requires TurboModules, but the new architecture is not enabled!' +
-          BULLET_POINT +
-          'Downgrade to react-native-mmkv 2.x.x if you want to stay on the old architecture.' +
-          BULLET_POINT +
-          'Enable the new architecture in your app to use react-native-mmkv 3.x.x. (See https://github.com/reactwg/react-native-new-architecture/blob/main/docs/enable-apps.md)'
+      const message =
+        'Failed to create a new MMKV instance: react-native-mmkv 3.x.x requires TurboModules, but the new architecture is not enabled!';
+      const suggestions: string[] = [];
+      suggestions.push(
+        'Downgrade to react-native-mmkv 2.x.x if you want to stay on the old architecture.'
       );
+      suggestions.push(
+        'Enable the new architecture in your app to use react-native-mmkv 3.x.x. (See https://github.com/reactwg/react-native-new-architecture/blob/main/docs/enable-apps.md)'
+      );
+      const error = messageWithSuggestions(message, suggestions);
+      super(error, { cause: cause });
+      return;
+    }
+
+    const framework = getFrameworkType();
+    if (framework === 'expo-go') {
+      super(
+        'react-native-mmkv is not supported in Expo Go! Use EAS (`expo prebuild`) or eject to a bare workflow instead.'
+      );
+      return;
     }
 
     const message =
@@ -36,6 +72,9 @@ export class ModuleNotFoundError extends Error {
       'Make sure you are using react-native 0.74.0 or higher, because react-native-mmkv is a C++ TurboModule.'
     );
     suggestions.push('Make sure you rebuilt the app.');
+    if (framework === 'expo') {
+      suggestions.push('Make sure you ran `expo prebuild`.');
+    }
     switch (Platform.OS) {
       case 'ios':
       case 'macos':
@@ -49,22 +88,8 @@ export class ModuleNotFoundError extends Error {
       default:
         throw new Error(`MMKV is not supported on ${Platform.OS}!`);
     }
-    // check if Expo
-    const ExpoConstants =
-      NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants;
-    if (ExpoConstants != null) {
-      if (ExpoConstants.appOwnership === 'expo') {
-        // We're running Expo Go
-        throw new Error(
-          'react-native-mmkv is not supported in Expo Go! Use EAS (`expo prebuild`) or eject to a bare workflow instead.'
-        );
-      } else {
-        // We're running Expo bare / standalone
-        suggestions.push('Make sure you ran `expo prebuild`.');
-      }
-    }
 
-    const error = message + BULLET_POINT + suggestions.join(BULLET_POINT);
+    const error = messageWithSuggestions(message, suggestions);
     super(error, { cause: cause });
   }
 }
