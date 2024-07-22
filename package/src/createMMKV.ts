c4fe9f19e9a9deb076115e29cb51973d29510472
Marc Rousavy <me@mrousavy.com>
2024-07-22 17:05:22 +0200

    Update createMMKV.ts

--- package/src/createMMKV.ts
+++ package/src/createMMKV.ts
@@ -8,12 +8,17 @@ export const createMMKV = (config: Configuration): NativeMMKV => {
 
   if (Platform.OS === 'ios') {
     if (config.path == null) {
-      // If no `path` was supplied, we check if an `AppGroup` was set in Info.plist
-      const appGroupDirectory =
-        getMMKVPlatformContextTurboModule().getAppGroupDirectory();
-      if (appGroupDirectory != null) {
-        // If we have an `AppGroup` in Info.plist, use that as a path.
-        config.path = appGroupDirectory;
+      try {
+        // If no `path` was supplied, we check if an `AppGroup` was set in Info.plist
+        const appGroupDirectory =
+          getMMKVPlatformContextTurboModule().getAppGroupDirectory();
+        if (appGroupDirectory != null) {
+          // If we have an `AppGroup` in Info.plist, use that as a path.
+          config.path = appGroupDirectory;
+        }
+      } catch (e) {
+        // We cannot throw errors here because it is a sync C++ TurboModule func. idk why.
+        console.error(e);
       }
     }
   }
