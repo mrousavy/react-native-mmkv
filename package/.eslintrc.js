3d9649bea40022919b1f8525a62f63c01e60e65a
Marc Rousavy <me@mrousavy.com>
2024-08-29 17:38:04 +0200

    Update .eslintrc.js

--- package/.eslintrc.js
+++ package/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: ['@react-native', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   ignorePatterns: ['scripts', 'lib'],
   rules: {
     'prettier/prettier': ['warn'],
