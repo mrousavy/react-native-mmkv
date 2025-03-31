67a474d69d9870057dac1159d1021a5fb9cc9490
Ilya Pakhomov <ipakhomov@ronasit.com>
2025-03-31 22:16:52 +0600

    docs: better comments in types tests

--- package/src/__tests__/mmkv.test-d.ts
+++ package/src/__tests__/mmkv.test-d.ts
@@ -18,37 +18,45 @@ export type TestStorageValues = {
   someBuffer: ArrayBuffer;
 };
 
-const typedStorage = new MMKV<TestStorageValues>();
-const nonTypedStorage = new MMKV();
+// ===== MMKV Instance Tests =====
 
-// Types test for non-typed storage:
+// Non-typed storage tests
+const nonTypedStorage = new MMKV();
 expectType<void>(nonTypedStorage.set('someRandomKey', 'some-token'));
 expectType<string | undefined>(nonTypedStorage.getString('someOtherKey'));
 
-// Valid type usages for typed storage:
+// Typed storage tests
+const typedStorage = new MMKV<TestStorageValues>();
+
+// Valid type usages for typed storage
+// String operations
 typedStorage.set('someString', 'some-token');
 expectType<string | undefined>(typedStorage.getString('someString'));
 
+// Number operations
 typedStorage.set('someNumber', 1);
 typedStorage.set('someOtherNumber', 2);
 expectType<number | undefined>(typedStorage.getNumber('someNumber'));
 expectType<number | undefined>(typedStorage.getNumber('someOtherNumber'));
 
+// Boolean operations
 typedStorage.set('someBoolean', true);
 expectType<boolean | undefined>(typedStorage.getBoolean('someBoolean'));
 
+// Buffer operations
 typedStorage.set('someBuffer', new ArrayBuffer(10));
 expectType<ArrayBufferLike | undefined>(typedStorage.getBuffer('someBuffer'));
 
+// Utility operations
 expectType<(keyof TestStorageValues)[]>(typedStorage.getAllKeys());
 expectType<boolean>(typedStorage.contains('someString'));
 typedStorage.addOnValueChangedListener((key) => {
   expectType<keyof TestStorageValues>(key);
 });
 
-// Invalid type usages (should cause TypeScript errors):
+// Invalid type usages (should cause TypeScript errors)
 
-// These cases should fail because "fooBar" is not a valid key:
+// Invalid key tests
 expectError(typedStorage.set('fooBar', 'someString'));
 expectError(typedStorage.delete('fooBar'));
 expectError(typedStorage.contains('fooBar'));
@@ -57,35 +65,39 @@ expectError(typedStorage.getNumber('fooBar'));
 expectError(typedStorage.getBoolean('fooBar'));
 expectError(typedStorage.getBuffer('fooBar'));
 
-// These cases should fail because values are of the wrong type, while keys are valid:
+// Invalid value type tests
 expectError(typedStorage.set('someString', 123));
 expectError(typedStorage.set('someNumber', '123'));
 expectError(typedStorage.set('someBoolean', 123));
 expectError(typedStorage.set('someBuffer', '123'));
 
-// These cases should fail because methods expect keys of the correct type:
+// Invalid method for key type tests
 expectError(typedStorage.getString('someBoolean'));
 expectError(typedStorage.getBoolean('someString'));
 expectError(typedStorage.getNumber('someBoolean'));
 expectError(typedStorage.getBuffer('someString'));
 
-// Tests for hooks types.
+// ===== Hooks Tests =====
 
-// Tests for useMMKV hook
+// useMMKV hook tests
 const localTypedStorage = useMMKV<TestStorageValues>();
 expectType<void>(localTypedStorage.set('someString', 'some-token'));
 expectType<string | undefined>(localTypedStorage.getString('someString'));
 expectType<(keyof TestStorageValues)[]>(localTypedStorage.getAllKeys());
 
-// Valid type usages for typed storage with hooks:
+// Valid hook usages
+
+// String hook
 const [someString, setSomeString] = useMMKVString('someString', typedStorage);
 expectType<string | undefined>(someString);
 expectType<void>(setSomeString('some-token'));
 
+// Number hook
 const [someNumber, setSomeNumber] = useMMKVNumber('someNumber', typedStorage);
 expectType<number | undefined>(someNumber);
 expectType<void>(setSomeNumber(1));
 
+// Boolean hook
 const [someBoolean, setSomeBoolean] = useMMKVBoolean(
   'someBoolean',
   typedStorage
@@ -93,10 +105,12 @@ const [someBoolean, setSomeBoolean] = useMMKVBoolean(
 expectType<boolean | undefined>(someBoolean);
 expectType<void>(setSomeBoolean(true));
 
+// Buffer hook
 const [someBuffer, setSomeBuffer] = useMMKVBuffer('someBuffer', typedStorage);
 expectType<ArrayBufferLike | undefined>(someBuffer);
 expectType<void>(setSomeBuffer(new ArrayBuffer(10)));
 
+// Object hook
 type SomeObject = {
   foo: string;
   bar: number;
@@ -110,21 +124,21 @@ const [someObject, setSomeObject] = useMMKVObject<
 expectType<SomeObject | undefined>(someObject);
 expectType<void>(setSomeObject({ foo: 'baz', bar: 2 }));
 
-// Invalid type usages for hooks:
+// Invalid hook usages
 
-// These cases should fail because the key does not match the type of hook value:
+// Invalid key for hook type tests
 expectError(useMMKVString('someBoolean', typedStorage));
 expectError(useMMKVNumber('someString', typedStorage));
 expectError(useMMKVBoolean('someNumber', typedStorage));
 expectError(useMMKVBuffer('someString', typedStorage));
 
-// These cases should fail because the key does not match the type of hook setter:
+// Invalid setter value type tests
 expectError(setSomeString(1));
 expectError(setSomeNumber('1'));
 expectError(setSomeBoolean(1));
 expectError(setSomeBuffer('1'));
 
-// These cases should fail becase keys are not present in the typed storage:
+// Non-existent key tests
 expectError(useMMKVString('someOtherKey', typedStorage));
 expectError(useMMKVNumber('someOtherKey', typedStorage));
 expectError(useMMKVBoolean('someOtherKey', typedStorage));
