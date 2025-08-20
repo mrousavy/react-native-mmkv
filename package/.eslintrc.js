cff08bd13c2ac32f2dbce5ddcf71f493ac417aec
Brad Anderson <brad@sankatygroup.com>
2025-08-20 09:39:33 -0400

    feat: Use MMKVCore from CocoaPods & Gradle Prefabs (#847)
    * feat: use pod and prefab
    
    * chore: update phone for gha runner
    
    * Update Podfile.lock
    
    * fix: Bump to C++20
    
    * fix: Remove `prefab` from Android example
    
    ---------
    
    Co-authored-by: Marc Rousavy <me@mrousavy.com>

--- package/.eslintrc.js
+++ package/.eslintrc.js
@@ -2,7 +2,14 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
-  ignorePatterns: ['scripts', 'lib'],
+  ignorePatterns: [
+    'build',
+    'scripts',
+    'lib',
+    '.eslintrc.js',
+    '.prettierrc.js',
+    '*.config.js',
+  ],
   rules: {
     'prettier/prettier': ['warn'],
   },
--- package/cpp/NativeMmkvModule.h
+++ package/cpp/NativeMmkvModule.h
@@ -7,15 +7,8 @@
 
 #pragma once
 
-#if __has_include(<React-Codegen/RNMmkvSpecJSI.h>)
-// CocoaPods include (iOS)
-#include <React-Codegen/RNMmkvSpecJSI.h>
-#elif __has_include(<RNMmkvSpecJSI.h>)
-// CMake include on Android
 #include <RNMmkvSpecJSI.h>
-#else
-#error Cannot find react-native-mmkv spec! Try cleaning your cache and re-running CodeGen!
-#endif
+#include <string>
 
 namespace facebook::react {
 
