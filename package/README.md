d00458191c6d62373304083f3dcf0853ca3ab9dd
Marc Rousavy <me@mrousavy.com>
2025-08-21 15:15:53 +0200

    Remove current `package/`

--- package/README.md
+++
@@ -1,5 +0,0 @@
-# react-native-mmkv
-
-The fastest key/value storage for React Native. ~30x faster than AsyncStorage!
-
-See [README.md](https://github.com/mrousavy/react-native-mmkv) for more information.
--- package/src/PlatformChecker.ts
+++
@@ -1,9 +0,0 @@
-export function isTest(): boolean {
-  if (global.process == null) {
-    // In a WebBrowser/Electron the `process` variable does not exist
-    return false;
-  }
-  return (
-    process.env.JEST_WORKER_ID != null || process.env.VITEST_WORKER_ID != null
-  );
-}
