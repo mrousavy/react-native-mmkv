c2886347b24d0e5257f4c4952be855bd23f1e54e
Marc Rousavy <me@mrousavy.com>
2025-08-21 17:44:01 +0200

    Update format

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+set -e
+
+CPP_DIRS=(
+  # react-native-mmkv
+  "packages/react-native-mmkv/android/src/main/cpp"
+  "packages/react-native-mmkv/cpp"
+  "packages/react-native-mmkv/ios"
+)
+
 if which clang-format >/dev/null; then
-  cd packages/react-native-mmkv
-  find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.m" -o -name "*.mm" \) -print0 | while read -d $'\0' file; do
-    echo "-> cpp-lint $file"
-    clang-format -i "$file"
+  DIRS=$(printf "%s " "${CPP_DIRS[@]}")
+  find $DIRS -type f \( -name "*.h" -o -name "*.hpp" -o -name "*.cpp" -o -name "*.m" -o -name "*.mm" -o -name "*.c" \) -print0 | while read -d $'\0' file; do
+    clang-format -style=file:./.clang-format -i "$file"
   done
 else
-  echo "warning: clang-format not installed, download from https://clang.llvm.org/docs/ClangFormat.html (or run brew install clang-format)"
+  echo "error: clang-format not installed, install with 'brew install clang-format' (or manually from https://clang.llvm.org/docs/ClangFormat.html)"
+  exit 1
 fi
