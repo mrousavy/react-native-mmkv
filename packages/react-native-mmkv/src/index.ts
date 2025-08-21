f4c05f3636f8e2d7790d37a70425266bb4729717
Marc Rousavy <me@mrousavy.com>
2025-08-21 15:49:15 +0200

    Set up example

--- packages/react-native-mmkv/src/index.ts
+++ packages/react-native-mmkv/src/index.ts
@@ -1,1 +1,14 @@
-// TODO: Export all HybridObjects here for the user
+import { NitroModules } from 'react-native-nitro-modules'
+import { type MMKVFactory, type Configuration } from './specs/MMKVFactory.nitro'
+import type { MMKV } from './specs/MMKV.nitro'
+
+const HybridMMKVFactory =
+  NitroModules.createHybridObject<MMKVFactory>('MMKVFactory')
+
+export { type MMKV, type Configuration }
+
+export function createMMKV(): MMKV
+export function createMMKV(configuration: Configuration): MMKV
+export function createMMKV(configuration?: Configuration): MMKV {
+  return HybridMMKVFactory.createMMKV(configuration ?? { id: 'mmkv.default' })
+}
