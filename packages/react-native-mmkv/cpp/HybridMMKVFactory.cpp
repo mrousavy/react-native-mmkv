3e48f2788dea7a971c998da6728e6bdf74b5436b
Marc Rousavy <me@mrousavy.com>
2025-08-21 15:27:08 +0200

    Add files

---
+++ packages/react-native-mmkv/cpp/HybridMMKVFactory.cpp
@@ -0,0 +1,8 @@
+//
+//  HybridMMKVFactory.cpp
+//  react-native-mmkv
+//
+//  Created by Marc Rousavy on 21.08.2025.
+//
+
+#include "HybridMMKVFactory.hpp"
---
+++ packages/react-native-mmkv/cpp/HybridMMKVFactory.hpp
@@ -0,0 +1,16 @@
+//
+//  HybridMMKVFactory.hpp
+//  react-native-mmkv
+//
+//  Created by Marc Rousavy on 21.08.2025.
+//
+
+#pragma once
+
+namespace margelo::nitro::mmkv {
+
+  class HybridMMKVFactory: HybridMMKVFactorySpec {
+
+  };
+
+}
