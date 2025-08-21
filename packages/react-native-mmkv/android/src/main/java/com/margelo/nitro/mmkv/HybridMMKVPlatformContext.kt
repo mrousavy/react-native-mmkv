aff766a5a93334785b5cac51961ba031f92a18cf
Marc Rousavy <me@mrousavy.com>
2025-08-21 18:11:47 +0200

    feat: Implement Android platform context

---
+++ packages/react-native-mmkv/android/src/main/java/com/margelo/nitro/mmkv/HybridMMKVPlatformContext.kt
@@ -0,0 +1,19 @@
+package com.margelo.nitro.mmkv
+
+import androidx.annotation.Keep
+import com.facebook.common.internal.DoNotStrip
+import com.margelo.nitro.NitroModules
+
+@DoNotStrip
+@Keep
+class HybridMMKVPlatformContext: HybridMMKVPlatformContextSpec() {
+    override fun getBaseDirectory(): String {
+        val context = NitroModules.applicationContext ?: throw Error("Cannot get MMKV base directory - No Android Context available!")
+        return context.filesDir.absolutePath + "/mmkv";
+    }
+
+    override fun getAppGroupDirectory(): String {
+        // AppGroups do not exist on Android. It's iOS only.
+        throw Error("getAppGroupDirectory() is not supported on Android! It's iOS only.")
+    }
+}
