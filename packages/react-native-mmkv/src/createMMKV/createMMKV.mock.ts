e58149d1e0edf6c58fd33a6cef665a1af37f47ff
Marc Rousavy <me@mrousavy.com>
2025-08-28 23:35:50 +0200

    chore: Fix `delete`

--- packages/react-native-mmkv/src/createMMKV/createMMKV.mock.ts
+++ packages/react-native-mmkv/src/createMMKV/createMMKV.mock.ts
@@ -9,7 +9,7 @@ export function createMockMMKV(): MMKV {
 
   return {
     clearAll: () => storage.clear(),
-    remove: (key) => storage.remove(key),
+    remove: (key) => storage.delete(key),
     set: (key, value) => storage.set(key, value),
     getString: (key) => {
       const result = storage.get(key)
