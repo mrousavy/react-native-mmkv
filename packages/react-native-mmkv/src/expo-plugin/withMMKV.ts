9b0f74db6028a3f460a93a75937157e6b4d275d5
leoboyerbx <contact@leoboyer.dev>
2025-09-04 17:07:01 +0200

    feat: improve Expo plugin to edit arch property in-place

--- packages/react-native-mmkv/src/expo-plugin/withMMKV.ts
+++ packages/react-native-mmkv/src/expo-plugin/withMMKV.ts
@@ -6,16 +6,23 @@ const pkg = require('../../package.json')
 const withMMKV: ConfigPlugin<{}> = (config) => {
   // remove 32-bit architectures from gradle.properties
   return withGradleProperties(config, (cfg) => {
-    // Drop any existing entry…
-    cfg.modResults = cfg.modResults.filter(
-      (p) => !(p.type === 'property' && p.key === 'reactNativeArchitectures')
-    )
-    // …and force 64-bit only.
-    cfg.modResults.push({
+    // Define the wanted property
+    const property = {
       type: 'property',
       key: 'reactNativeArchitectures',
       value: 'arm64-v8a,x86_64',
-    })
+    } as const
+    // If it exists, update its value
+    const index = cfg.modResults.findIndex(
+      (p) => p.type === 'property' && p.key === property.key
+    )
+    if (index !== -1) {
+      cfg.modResults[index] = property
+    }
+    // Else add it to the properties
+    else {
+      cfg.modResults.push(property)
+    }
     return cfg
   })
 }
