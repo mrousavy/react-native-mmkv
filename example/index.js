b672be6683b4e26f2b4b0d5495ca7052e1ff2ef6
Szymon Chmal <szymon@chmal.it>
2025-10-14 11:27:41 +0200

    refactor: use correct entry point

--- example/index.js
+++ example/index.js
@@ -6,8 +6,7 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () =>
-    global.RN_HARNESS
-      ? require('@react-native-harness/runtime').ReactNativeHarness
-      : require('./src/App').default
-  );
-  
+  global.RN_HARNESS
+    ? require('react-native-harness').ReactNativeHarness
+    : require('./src/App').default,
+);
