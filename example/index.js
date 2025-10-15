cbcb4bd0cc89a78a5f5a3a244d5248d28fe03275
Szymon Chmal <szymonchmal@gmail.com>
2025-10-15 12:57:47 +0200

    feat: add React Native Harness (#918)
    * feat: add Harness
    
    * chore: update harness
    
    * feat: turn off unstable optimization
    
    * refactor: move RCT flags to Podfile
    
    * chore: remove binary lockfile
    
    * refactor: use correct entry point
    
    * chore: update harness
    
    * fix: opt out from isolated installs
    
    * chore: remove no longer needed config

--- example/index.js
+++ example/index.js
@@ -3,7 +3,10 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () =>
+  global.RN_HARNESS
+    ? require('react-native-harness').ReactNativeHarness
+    : require('./src/App').default,
+);
--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path')
+const path = require('path');
+const { withRnHarness } = require('react-native-harness/metro');
 
 /**
  * Metro configuration
@@ -8,7 +9,9 @@ const path = require('path')
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
-  watchFolders: [path.resolve(__dirname, '..')]
+  watchFolders: [path.resolve(__dirname, '..')],
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withRnHarness(
+  mergeConfig(getDefaultConfig(__dirname), config),
+);
