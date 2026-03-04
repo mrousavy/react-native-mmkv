dd971b16a0a07e3716d39d60904e6d7923b1a37b
Marc Rousavy <me@mrousavy.com>
2026-03-04 14:40:21 +0100

    Update cpp-adapter for new `registerAllNatives()` API

--- packages/react-native-mmkv/android/src/main/cpp/cpp-adapter.cpp
+++ packages/react-native-mmkv/android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,9 @@
 #include "NitroMmkvOnLoad.hpp"
 #include <jni.h>
+#include <fbjni/fbjni.h>
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::mmkv::initialize(vm);
+  return facebook::jni::initialize(vm, []() {
+    margelo::nitro::mmkv::registerAllNatives();
+  });
 }
