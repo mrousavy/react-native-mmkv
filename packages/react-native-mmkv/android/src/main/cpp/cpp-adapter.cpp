398c1f80e68f8509de696d6c337f2047548bfbb1
Marc Rousavy <me@mrousavy.com>
2026-03-04 14:40:47 +0100

    chore: Lint/format

--- packages/react-native-mmkv/android/src/main/cpp/cpp-adapter.cpp
+++ packages/react-native-mmkv/android/src/main/cpp/cpp-adapter.cpp
@@ -1,9 +1,7 @@
 #include "NitroMmkvOnLoad.hpp"
-#include <jni.h>
 #include <fbjni/fbjni.h>
+#include <jni.h>
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return facebook::jni::initialize(vm, []() {
-    margelo::nitro::mmkv::registerAllNatives();
-  });
+  return facebook::jni::initialize(vm, []() { margelo::nitro::mmkv::registerAllNatives(); });
 }
